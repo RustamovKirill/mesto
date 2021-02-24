dde3b5f087f3e494e71f7acb99237d465cdd3cf1
Kirill Rustamov <aloai@yandex.ru>
2021-02-24 10:04:56 +0300

    первый commit

---
+++ script/script.js
@@ -0,0 +1,32 @@
+let openForm = document.querySelector('#open-form'); //переменная кнопки открытия popup
+let closeForm = document.querySelector('.popup__button'); //переменная кнопки закрытия popup
+let saveButton = document.querySelector('.input__button-save'); //переменная кнопки сохранить
+let popup = document.querySelector('.popup'); //переменная с блоком popup
+let profileTitle = document.querySelector('.profile__title'); //
+let profileSubtitle = document.querySelector('.profile__subtitle');//
+
+//функция запускающая popup
+function openPopup() {
+    popup.classList.add('popup_open');
+    document.querySelector('.input__text_type_name').value = profileTitle.textContent;
+    document.querySelector('.input__text_type_content').value = profileSubtitle.textContent;
+}
+//функция закрывающая popup
+function closePopup() {
+    popup.classList.remove('popup_open');
+}
+document.querySelector('.input').addEventListener('submit', function (event) {
+    event.preventDefault();
+    profileTitle.textContent = document.querySelector('.input__text_type_name').value;
+    profileSubtitle.textContent = document.querySelector('.input__text_type_content').value;
+});
+// обработка нажатия кнопкой Enter
+saveButton.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) {
+        closePopup();
+    }
+});
+openForm.addEventListener('click', openPopup); //дейсвие по нажатию на кнопку открытия ручка
+closeForm.addEventListener('click', closePopup); //дейсвие по нажатию на кнопку Х, закрытие формы
+saveButton.addEventListener('click', closePopup);//дейсвие по нажатию на кнопку Сохранить
+
