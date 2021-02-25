88826745ab35ad03c93791a642beaf20b622cacb
Kirill Rustamov <aloai@yandex.ru>
2021-02-25 18:01:46 +0300

    refactor: изменил классы, переписал js, поправил css

--- script/script.js
+++ script/script.js
@@ -1,31 +1,34 @@
 let openForm = document.querySelector('#open-form'); //переменная кнопки открытия popup
-let closeForm = document.querySelector('.popup__button'); //переменная кнопки закрытия popup
-let saveButton = document.querySelector('.input__button-save'); //переменная кнопки сохранить
-let popup = document.querySelector('.popup'); //переменная с блоком popup
-let profileTitle = document.querySelector('.profile__title'); //поле с именем
-let profileSubtitle = document.querySelector('.profile__subtitle');//поле с описанием
+let closeForm = document.querySelector('.popup__button');//переменная кнопки закрытия popup
+let popup = document.querySelector('.popup');//объявляем переменную с блоком popup
+//переменная с полем имени, тут будет отображаться текст веденный в поле textName
+let profileTitle = document.querySelector('.profile__title');
+//переменная с полем, где будет отображться информация записанная в переменную textSubtitle
+let profileSubtitle = document.querySelector('.profile__subtitle');
+//переменная содержащая значения в блоке с классом input
+let inputs = document.querySelector('.input');
+//объявляем переменную с полем ввода имени
+let textName = document.querySelector('.input__text_type_name');
+//объявляем переменную с полем ввода описания
+let textSubtitle = document.querySelector('.input__text_type_content');
 
-//функция запускающая popup
-function openPopup() {
+//функция открывающая popup и отображает значения, которые отображаються на странице
+function openPopup(event) {
     popup.classList.add('popup_open');
-    document.querySelector('.input__text_type_name').value = profileTitle.textContent;
-    document.querySelector('.input__text_type_content').value = profileSubtitle.textContent;
+    textName.value = profileTitle.textContent;
+    textSubtitle.value = profileSubtitle.textContent;
 }
-//функция закрывающая popup
+//функция закрывающая popup, потем удаления модификатора класса popup_open
 function closePopup() {
     popup.classList.remove('popup_open');
 }
-document.querySelector('.input').addEventListener('submit', function (event) {
+//функция отправляет изменения, которые были внесены в поля «Имя» и «О себе»
+function form(event) {
     event.preventDefault();
-    profileTitle.textContent = document.querySelector('.input__text_type_name').value;
-    profileSubtitle.textContent = document.querySelector('.input__text_type_content').value;
-});
-// обработка нажатия кнопкой Enter
-saveButton.addEventListener('keypress', function (event) {
-    if (event.keyCode === 13) {
-        closePopup();
-    }
-});
-openForm.addEventListener('click', openPopup); //дейсвие по нажатию на кнопку открытия ручка
+    profileTitle.textContent = textName.value;
+    profileSubtitle.textContent = textSubtitle.value;
+    closePopup();
+}
+inputs.addEventListener('submit', form);//запуск функии form после обновления значения 
+openForm.addEventListener('click', openPopup); //слушатель открытия формы
 closeForm.addEventListener('click', closePopup); //дейсвие по нажатию на кнопку Х, закрытие формы
-saveButton.addEventListener('click', closePopup);//дейсвие по нажатию на кнопку Сохранить
